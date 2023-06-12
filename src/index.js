b83fb01aff0c9a7fcb7a44f3bf2acd6930703142
Brian Paintsil <brian@gng.org>
2023-06-12 21:59:38 +0000

    add analytics

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from "./reportWebVitals";
 import { store } from "./store";
 import { Provider } from "react-redux";
 import { Auth0Provider } from "@auth0/auth0-react";
+import { Analytics } from '@vercel/analytics/react';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -17,6 +18,7 @@ root.render(
     >
       <Provider store={store}>
         <App />
+        <Analytics />
       </Provider>
     </Auth0Provider>
   </React.StrictMode>
